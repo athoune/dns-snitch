0e95014c8f9140160dddc88bbc498861ba119604
Mathieu Lecarme <mathieu@garambrogne.net>
2024-11-17 22:50:43 +0100

    feat LeakyBucket is now thread safe

--- bucket/bucket.go
+++ bucket/bucket.go
@@ -4,21 +4,26 @@ import (
 	"fmt"
 	"io"
 	"log/slog"
+	"sync"
 )
 
 type LeakyBucket[K comparable] struct {
 	datas    map[K]*BucketValues
 	capacity int
+	lock     *sync.RWMutex
 }
 
 func NewLeakyBucket[K comparable](capacity int) *LeakyBucket[K] {
 	return &LeakyBucket[K]{
 		datas:    make(map[K]*BucketValues),
 		capacity: capacity,
+		lock:     &sync.RWMutex{},
 	}
 }
 
 func (l *LeakyBucket[K]) Get(line K) *BucketValues {
+	l.lock.RLock()
+	defer l.lock.RUnlock()
 	v, ok := l.datas[line]
 	if ok {
 		return v
@@ -27,6 +32,8 @@ func (l *LeakyBucket[K]) Get(line K) *BucketValues {
 }
 
 func (l *LeakyBucket[K]) Dump(out io.Writer) {
+	l.lock.RLock()
+	defer l.lock.RUnlock()
 	for k, v := range l.datas {
 		fmt.Fprint(out, k)
 		fmt.Fprintln(out, " => ", v.Sum(), "\n", v)
@@ -34,6 +41,8 @@ func (l *LeakyBucket[K]) Dump(out io.Writer) {
 }
 
 func (l *LeakyBucket[K]) LeaksAll() {
+	l.lock.Lock()
+	defer l.lock.Unlock()
 	before := len(l.datas)
 	olds := make([]K, 0)
 	for k, v := range l.datas {
@@ -49,6 +58,8 @@ func (l *LeakyBucket[K]) LeaksAll() {
 }
 
 func (l *LeakyBucket[K]) Add(line K, value int) {
+	l.lock.Lock()
+	defer l.lock.Unlock()
 	current, ok := l.datas[line]
 	if ok {
 		current.Add(value)
@@ -59,6 +70,8 @@ func (l *LeakyBucket[K]) Add(line K, value int) {
 }
 
 func (l *LeakyBucket[K]) Values() ([]K, []int) {
+	l.lock.RLock()
+	defer l.lock.RUnlock()
 	kk := make([]K, len(l.datas))
 	vv := make([]int, len(l.datas))
 	i := 0
